6e18ec0f9d2d8fcac9ca0fd163c23d7dc3f51366
clopylol <berkay.ertugrul@hotmail.com>
2021-05-06 22:10:51 +0300

    parser created

--- basic.py
+++ basic.py
@@ -1,3 +1,7 @@
+from string_with_arrows import *
+
+
+
 ###################################################
 ################# S A B İ T L E R #################
 ###################################################
@@ -19,13 +23,20 @@ class Error:
     def as_string(self):
         result = f'{self.error_name}: {self.error_details}'
         result += f'Dosya: {self.position_start.fileName}, Satır: {self.position_start.line + 1}'
+        result += '\n\n' + string_with_arrows(self.position_start.ftxt, self.position_start, self.position_end)
         return result
 
 class IllegalCharError(Error):
     def __init__(self, position_start, position_end, error_details):
-        super().__init__(position_start, position_end, 'Geçersiz Karakter', error_details)
-        
+        super().__init__(position_start, position_end, 'Geçersiz Karakter', error_details) # Illegal Character
 
+class InvalidSyntaxError(Error):
+    # IllegalSyntax
+    def __init__(self, position_start, position_end, error_details):
+	    super().__init__(position_start, position_end, 'Geçesiz Syntax', error_details) 
+
+
+    
 ###################################################
 ################# P O S I T I O N #################
 #Convert = idx, ln, col
@@ -75,14 +86,22 @@ TT_RPAREN = 'RPAREN' # Sağ Parantez )
 
 
 class Token:
-    def __init__(self, type_, value=None):
+    def __init__(self, type_, value=None, position_start = None, position_end = None):
         self.type = type_
         self.value = value
 
+    if position_start:
+        self.position_start = position_start.copy()
+        self.position_end = position_start.copy()
+        self.position_end.advance()
+
+    if position_end:
+        self.position_end = position_end
+
     #Temsil
-    def __repr__(self):
-        if self.value: return f'{self.type}:{self.value}'
-        return f'{self.type}'
+def __repr__(self):
+    if self.value: return f'{self.type}:{self.value}'
+    return f'{self.type}'
 
 
 ###################################################
@@ -171,15 +190,117 @@ class Lexer:
 
         return tokens, None
 
+###################################################
+#################### N O D E S ####################
+###################################################
+
+class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+
+    def __repr__(self):
+        return '${self.tok}' # f'{self.tok}'
+
+class BinOpNode:
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+
+    def __repr__(self):
+        return (self.left_node, self.op_tok, self.right_node)
+
+
+###################################################
+############ P A R S E R  R E S U L T #############
+###################################################
+
+class ParseResult:
+    def __init__(self):
+        self.error = None
+        self.node = None
+
+    def register(self, res):
+        if isinstance(res, ParseResult):
+            if res.error : 
+                self.error = res.error
+                return res.node
+
+        return res
+
+    def success(self, node):    
+        self.node = node 
+        return self
+
+    def failure(self, error): 
+        self.error = error 
+        return self
+
+###################################################
+#################### P A R S E R ##################
+###################################################
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_idx = 1
+        self.advance()
+    
+    def advance(self):
+        self.tok_idx += 1
+
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+
+######################################################
+    def parse(self):
+        res = self.expr()
+        return res
+
+    def factor(self,):
+        tok = self.current_tok
+
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    def expr(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    #####################################################
+
+    def bin_op(self, func, ops):
+        left = func()
+
+        while self.current_tok.type in ops:
+            op_tok = self.current_tok
+            self.advance()
+            right = func()
+            left = BinOpNode(left, op_tok, right)
+
+        return left
 
 ###################################################
 ################# Ç A L I Ş T I R #################
 ###################################################
 
 def run(fileName,text):
+
+    # Tokenlerı Üretelim
     lexer = Lexer(fileName, text)
     tokens, error = lexer.make_tokens()
 
-    return tokens, error
+    if error: return None, error
+
+    # AST'leri Üret
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+
+    return ast, None
 
 
