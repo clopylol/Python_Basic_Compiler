a09330a29449eee912a8713499ed32202823a703
clopylol <berkay.ertugrul@hotmail.com>
2021-05-29 20:05:59 +0300

    parser and lexer edit.

--- basic.py
+++ basic.py
@@ -1,19 +1,18 @@
 from string_with_arrows import *
 
-
-
 ###################################################
 ################# S A B İ T L E R #################
 ###################################################
 
-#Rakamlar
+# Rakamlar
 DIGITS = '0123456789'
 
 ###################################################
 ################# H A T A L A R ###################
 ###################################################
 
-class Error: 
+
+class Error:
     def __init__(self, position_start, position_end, error_name, error_details):
         self.position_start = position_start
         self.position_end = position_end
@@ -23,23 +22,27 @@ class Error:
     def as_string(self):
         result = f'{self.error_name}: {self.error_details}'
         result += f'Dosya: {self.position_start.fileName}, Satır: {self.position_start.line + 1}'
-        result += '\n\n' + string_with_arrows(self.position_start.ftxt, self.position_start, self.position_end)
+        result += '\n\n' + \
+            string_with_arrows(self.position_start.ftxt,
+                               self.position_start, self.position_end)
         return result
+
 
 class IllegalCharError(Error):
     def __init__(self, position_start, position_end, error_details):
-        super().__init__(position_start, position_end, 'Geçersiz Karakter', error_details) # Illegal Character
+        super().__init__(position_start, position_end,
+                         'Geçersiz Karakter', error_details)  # Illegal Character
+
 
 class InvalidSyntaxError(Error):
     # IllegalSyntax
     def __init__(self, position_start, position_end, error_details):
-	    super().__init__(position_start, position_end, 'Geçesiz Syntax', error_details) 
+        super().__init__(position_start, position_end, 'Geçesiz Syntax', error_details)
 
 
-    
 ###################################################
-################# P O S I T I O N #################
-#Convert = idx, ln, col
+################# P O S I Z Y O N #################
+# Convert = idx, ln, col
 ###################################################
 
 class Position(object):
@@ -51,56 +54,66 @@ class Position(object):
         self.fileText = fileText
 
     # Satır İçerisinde ilerlemek için kullanacağız.
-    def advance(self, current_char):
+    def advance(self, current_char=None):
         self.index += 1
-        self.column +=1
+        self.column += 1
 
         if current_char == '\n':
-            self.line +=1
+            self.line += 1
             self.column = 0
 
         return self
 
     # Kullanıcıya hangi dosyanın hangi text'i içerisinde hangi satırda hatanın olduğunu gösterebileceğiz.
     def copy(self):
-       return Position(self.index, self.line, self.column, self.fileName, self.fileText)
-        
+        return Position(self.index, self.line, self.column, self.fileName, self.fileText)
+
 
 ###################################################
 ################# T O K E N L A R #################
 ###################################################
 
-# Keywords
-TT_INT = 'TT_INT' # int tipi
-TT_FLOAT = 'FLOAT' # float tipi
+# Değişken Tipi
+TT_INT = 'TT_INT'  # int tipi
+TT_FLOAT = 'FLOAT'  # float tipi
 
-# Operators
-TT_PLUS = 'PLUS' # Toplama (+) 
-TT_MINUS = 'MINUS' # Çıkarma (-) 
-TT_DIV = 'DIV' #Bölme (/)
-TT_MUL = 'MUL' #Çarpma  (*)
+# Operatörler
+TT_PLUS = 'PLUS'  # Toplama (+)
+TT_MINUS = 'MINUS'  # Çıkarma (-)
+TT_DIV = 'DIV'  # Bölme (/)
+TT_MUL = 'MUL'  # Çarpma  (*)
 
-# Special Symbols
-TT_LPAREN = 'LPAREN' # Sol Parantez (
-TT_RPAREN = 'RPAREN' # Sağ Parantez )
+# Özel Semboller
+TT_LPAREN = 'LPAREN'  # Sol Parantez (
+TT_RPAREN = 'RPAREN'  # Sağ Parantez )
+
+# Satır Sonu (end of line)
+TT_EOF = "EOF"
 
 
 class Token:
-    def __init__(self, type_, value=None, position_start = None, position_end = None):
+    def __init__(self, type_, value=None, position_start=None, position_end=None):
         self.type = type_
         self.value = value
 
-    if position_start:
-        self.position_start = position_start.copy()
-        self.position_end = position_start.copy()
-        self.position_end.advance()
+        if position_start:
+            self.position_start = position_start.copy()
+            self.position_end = position_start.copy()
+            self.position_end.advance()
 
-    if position_end:
-        self.position_end = position_end
+        if position_end:
+            self.position_end = position_end
 
-    #Temsil
+    def __repr__(self):
+        if self.value:
+            return f'{self.type}:{self.value}'
+        return f'{self.type}'
+    # Temsil
+
+
 def __repr__(self):
-    if self.value: return f'{self.type}:{self.value}'
+    if self.value:
+        return f'{self.type}:{self.value}'
     return f'{self.type}'
 
 
@@ -116,90 +129,102 @@ class Lexer:
         self.current_char = None
         self.advance()
 
-    # Line içerisinde ilerlemek için bu func. kullanacağız. 
+    # Line içerisinde ilerlemek için bu func. kullanacağız.
     def advance(self):
         # else durumuna düştüğümüzde satırın sonun gelmişiz demektir.
         # Girilen text içerisinde, text uzunluğu kadar gezeceğiz.
-        self.pos.advance(self.current_char) 
-        self.current_char = self.text[self.pos.index] if self.pos.index < len(self.text) else None
-
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.index] if self.pos.index < len(
+            self.text) else None
 
     def make_numbers(self):
         # int tipinde mi ? Yoksa float tipinde mi sayımız onu kontrol etmeliyiz.
         num_str = ''
-        dot_counter = 0
+        dot_counter = 0  # Nokta Sayısı
+        position_start = self.pos.copy()
 
         while self.current_char != None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
 
                 if dot_counter == 1:  # Sayı içerisinde birden fazla nokta olamaz.
-                    break 
+                    break
 
                 dot_counter += 1
                 num_str += '.'
-            else: 
+            else:
                 num_str += self.current_char
             self.advance()
 
-        # Sayıda hiç nokta yoksa tipi int'dır.   
-        if dot_counter == 0: 
-            return Token(TT_INT, int(num_str))
+        # Sayıda hiç nokta yoksa tipi int'dır.
+        if dot_counter == 0:
+            return Token(TT_INT, int(num_str), position_start, self.pos)
         # dot_counter'ın 0'dan farklı olduğu zaman sayıda nokta var demektir. Tipi floattır.
-        else: 
-            return Token(TT_FLOAT, float(num_str))
+        else:
+            return Token(TT_FLOAT, float(num_str), position_start, self.pos)
 
-
+    # Token Oluştur
     def make_tokens(self):
-        tokens = []    
-        #Okunan karakter boş (' ') olmadığı sürece:
+        tokens = []
+        # Okunan karakter boş (' ') olmadığı sürece:
         while self.current_char != None:
 
             # Boşluk varsa bir adım ilerle
-            if self.current_char in ' \t': 
+            if self.current_char in ' \t':
                 self.advance()
-           
-            elif self.current_char in DIGITS:
-               tokens.append(self.make_numbers())
 
-            elif self.current_char == '+': # Okunan karakter + ise bunu TT_PLUS tokeni olarak tutcağız.
-                tokens.append(Token(TT_PLUS)) # + karakterini token listemize TT_PLUS adıyla ekledik.
-                self.advance() # 1 Karekter ilerle.
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_numbers())
+
+            # Okunan karakter + ise bunu TT_PLUS tokeni olarak tutcağız.
+            elif self.current_char == '+':
+                # + karakterini token listemize TT_PLUS adıyla ekledik.
+                tokens.append(Token(TT_PLUS, position_start=self.pos))
+                self.advance()  # 1 Karekter ilerle.
             # Toplama işlemi için yaptığımız adımları, en üstte tanımladığımız tüm sabitler içinde yapalım...
+
+            # Çıkarma İşlemi
             elif self.current_char == '-':
-                tokens.append(Token(TT_MINUS))
+                tokens.append(Token(TT_MINUS, position_start=self.pos))
                 self.advance()
+            # Çarpma İşlemi
             elif self.current_char == '*':
-                tokens.append(Token(TT_MUL))
+                tokens.append(Token(TT_MUL, position_start=self.pos))
                 self.advance()
+            # Bölme İşlemi
             elif self.current_char == '/':
-                tokens.append(Token(TT_DIV))
+                tokens.append(Token(TT_DIV, position_start=self.pos))
                 self.advance()
+            # Sol Parantez
             elif self.current_char == '(':
-                tokens.append(Token(TT_LPAREN))
+                tokens.append(Token(TT_LPAREN, position_start=self.pos))
                 self.advance()
+            # Sağ Parantez
             elif self.current_char == ')':
-                tokens.append(Token(TT_RPAREN))
+                tokens.append(Token(TT_RPAREN, position_start=self.pos))
                 self.advance()
 
             # Eğer tanımlı olan karakterlerden hiç biri gelmediyse hata dönmemiz gerekiyor.
-            else: 
+            else:
                 position_start = self.pos.copy()
                 char = self.current_char
                 self.advance()
-                return [], IllegalCharError (position_start, self.pos,"'" + char + "'")
+                return [], IllegalCharError(position_start, self.pos, "'" + char + "'")
 
+        tokens.append(Token(TT_EOF, position_start=self.pos))
         return tokens, None
 
 ###################################################
 #################### N O D E S ####################
 ###################################################
 
+
 class NumberNode:
     def __init__(self, tok):
         self.tok = tok
 
     def __repr__(self):
-        return '${self.tok}' # f'{self.tok}'
+        return  f'{self.tok}'
+
 
 class BinOpNode:
     def __init__(self, left_node, op_tok, right_node):
@@ -222,30 +247,31 @@ class ParseResult:
 
     def register(self, res):
         if isinstance(res, ParseResult):
-            if res.error : 
+            if res.error:
                 self.error = res.error
-                return res.node
+            return res.node
 
         return res
 
-    def success(self, node):    
-        self.node = node 
+    def success(self, node):
+        self.node = node
         return self
 
-    def failure(self, error): 
-        self.error = error 
+    def failure(self, error):
+        self.error = error
         return self
 
 ###################################################
 #################### P A R S E R ##################
 ###################################################
 
+
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
         self.tok_idx = 1
         self.advance()
-    
+
     def advance(self):
         self.tok_idx += 1
 
@@ -256,14 +282,24 @@ class Parser:
 ######################################################
     def parse(self):
         res = self.expr()
+
+        # Hata yoksa ve Satır Sonu Değilse
+        if not res.error and self.current_tok.type != TT_EOF:
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.position_start, self.current_tok.position_end, "Hata: '+', '-', '*', veya '/' bekleniyor !"
+            ))
         return res
 
     def factor(self,):
+        res = ParseResult()
         tok = self.current_tok
 
         if tok.type in (TT_INT, TT_FLOAT):
-            self.advance()
-            return NumberNode(tok)
+            res.register(self.advance())
+            return res.success(NumberNode(tok))
+
+        return res.failure(InvalidSyntaxError(tok.position_start, tok.position_end, 'Float veya Int Bekleniyor !'
+                                              ))
 
     def term(self):
         return self.bin_op(self.factor, (TT_MUL, TT_DIV))
@@ -274,33 +310,38 @@ class Parser:
     #####################################################
 
     def bin_op(self, func, ops):
-        left = func()
+        res = ParseResult()
+        left = res.register(func())
+        if res.error:
+            return res
 
         while self.current_tok.type in ops:
             op_tok = self.current_tok
+            res.register(self.advance())
             self.advance()
-            right = func()
+            right = res.register(func())
+            if res.error:
+                return res
             left = BinOpNode(left, op_tok, right)
 
-        return left
+        return res.success(left)
 
 ###################################################
 ################# Ç A L I Ş T I R #################
 ###################################################
 
-def run(fileName,text):
+
+def run(fileName, text):
 
     # Tokenlerı Üretelim
     lexer = Lexer(fileName, text)
     tokens, error = lexer.make_tokens()
 
-    if error: return None, error
+    if error:
+        return None, error
 
-    # AST'leri Üret
+    # AST'leri Üret (Soyut Söz Dizimi)
     parser = Parser(tokens)
     ast = parser.parse()
 
-
-    return ast, None
-
-
+    return ast.node, ast.error
--- shell.py
+++ shell.py
@@ -1,7 +1,7 @@
 import basic
 
 while True:
-    text = input('basic > ')
+    text = input('test > ')
     #print(text)
 
     # FileName ve text parametre
